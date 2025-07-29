eef0a0bf833a22f8de063a680d951c32606fa669
nisha <nishacse123@gmail.com>
2025-07-29 11:59:47 +0530

    footer and linking with active navbar

--- wp-content/themes/custom/footer.php
+++ wp-content/themes/custom/footer.php
@@ -1,107 +1,140 @@
-
 <div class="footers-wrapper p-rel w-full">
-        <div class="oe-upper-triangle bx-border-box wds-tgl"></div>
+  <div class="oe-upper-triangle bx-border-box wds-tgl"></div>
 
-        <div class="footer-headings wds">
-          <h1 class="footer-heading">Locate Us At</h1>
-          <h1 class="footer-heading enquiry-heading-lg d-b">
-            Send your enquiry at
-          </h1>
-        </div>
+  <div class="footer-headings wds">
+    <div class="footer-left-header">
+      <h1 class="footer-heading">Send your enquiry at</h1>
+      <p class="footer-text">Subscribe to our newsletter to grt updates to our latest collections</p>
+    </div>
+    <div class="email-input p-rel mb-12">
+      <input placeholder="Enter your email" class="input-wds" type="text" />
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="user">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9849 15.3477C8.11731 15.3477 4.81445 15.9324 4.81445 18.2743C4.81445 20.6162 8.09636 21.2219 11.9849 21.2219C15.8525 21.2219 19.1545 20.6362 19.1545 18.2953C19.1545 15.9543 15.8735 15.3477 11.9849 15.3477Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9849 12.0078C14.523 12.0078 16.5801 9.94974 16.5801 7.41164C16.5801 4.87355 14.523 2.81641 11.9849 2.81641C9.44679 2.81641 7.3887 4.87355 7.3887 7.41164C7.38013 9.94117 9.42394 11.9993 11.9525 12.0078H11.9849Z" stroke="white" stroke-width="1.42857" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+      <button class="primary-button">Subscribe</button>
+    </div>
+  </div>
 
-        <div class="footer mt-40 wds">
-          <div class="location-india">
-            <!-- <h1 class="footer-heading">Locate Us At</h1> -->
+  <div class="footer wds">
 
-            <div class="address">
-              <img src="<?php echo get_template_directory_uri(); ?>/images/india-address.svg" alt="" />
-              <p class="address-content">
-                Phase 9 Industrial Area <br />Mohali, 160062 <br />India
-              </p>
-            </div>
-          </div>
+    <div class="service-info">
+      <div class="logo">
+        <img src="<?php echo get_template_directory_uri() ?>/images/arcs-logo-III.svg" alt="Site Logo" />
+      </div>
+      <p class="footer-para-text mb-28">We're a leading development company that creates web apps, mobile apps, and AI-powered solutions for iOS, Android, and the web. Our team handles everything — from frontend and backend to complete development. We deliver on time, write clean code, and build exactly what your business needs.</p>
+      <div class="info mb-8">
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9.00099 1.5C13.1432 1.5 16.501 4.85775 16.501 9C16.501 13.1422 13.1432 16.5 9.00099 16.5C7.67555 16.5023 6.37342 16.1515 5.22849 15.4838L1.50399 16.5L2.51799 12.774C1.84971 11.6287 1.49867 10.326 1.50099 9C1.50099 4.85775 4.85874 1.5 9.00099 1.5ZM6.44499 5.475L6.29499 5.481C6.19801 5.48768 6.10325 5.51315 6.01599 5.556C5.93467 5.60213 5.86041 5.65972 5.79549 5.727C5.70549 5.81175 5.65449 5.88525 5.59974 5.9565C5.32233 6.31718 5.17297 6.75998 5.17524 7.215C5.17674 7.5825 5.27274 7.94025 5.42274 8.27475C5.72949 8.95125 6.23424 9.6675 6.90024 10.3312C7.06074 10.491 7.21824 10.6515 7.38774 10.8008C8.21531 11.5293 9.20145 12.0547 10.2677 12.3353L10.6937 12.4005C10.8325 12.408 10.9712 12.3975 11.1107 12.3907C11.3291 12.3792 11.5424 12.3201 11.7355 12.2175C11.8336 12.1668 11.9295 12.1117 12.0227 12.0525C12.0227 12.0525 12.0545 12.031 12.1165 11.985C12.2177 11.91 12.28 11.8568 12.364 11.769C12.427 11.704 12.4795 11.6285 12.5215 11.5425C12.58 11.4203 12.6385 11.187 12.6625 10.9927C12.6805 10.8442 12.6752 10.7633 12.673 10.713C12.67 10.6328 12.6032 10.5495 12.5305 10.5143L12.094 10.3185C12.094 10.3185 11.4415 10.0343 11.0425 9.85275C11.0007 9.83457 10.956 9.82415 10.9105 9.822C10.8592 9.81663 10.8073 9.82236 10.7584 9.83879C10.7095 9.85523 10.6647 9.88198 10.627 9.91725C10.6232 9.91575 10.573 9.9585 10.0307 10.6155C9.99961 10.6573 9.95674 10.6889 9.90759 10.7063C9.85844 10.7237 9.80522 10.726 9.75474 10.713C9.70586 10.7 9.65798 10.6834 9.61149 10.6635C9.51849 10.6245 9.48624 10.6095 9.42249 10.5825C8.99189 10.3949 8.59332 10.1411 8.24124 9.83025C8.14674 9.74775 8.05899 9.65775 7.96899 9.57075C7.67395 9.28816 7.4168 8.96849 7.20399 8.61975L7.15974 8.5485C7.12844 8.50035 7.10277 8.44876 7.08324 8.39475C7.05474 8.2845 7.12899 8.196 7.12899 8.196C7.12899 8.196 7.31124 7.9965 7.39599 7.8885C7.47849 7.7835 7.54824 7.6815 7.59324 7.60875C7.68174 7.46625 7.70949 7.32 7.66299 7.20675C7.45299 6.69375 7.23599 6.1835 7.01199 5.676C6.96774 5.5755 6.83649 5.5035 6.71724 5.48925C6.67674 5.48425 6.63624 5.48025 6.59574 5.47725C6.49503 5.47147 6.39406 5.47248 6.29349 5.48025L6.44499 5.475Z" fill="#202323" />
+        </svg>
+        <p class="inner-content">+91(172)315-7744</p>
+      </div>
 
-          <div class="location-america">
-            <div class="address">
-              <img src="<?php echo get_template_directory_uri(); ?>/images/america.svg" alt="" />
-              <p class="address-content">
-                30 N Gould Street Ste <br />R Sheridan <br />Wyoming 82801
-                <br />US
-              </p>
-            </div>
-          </div>
 
-          <div class="location-germany">
-            <div class="address">
-              <img src="<?php echo get_template_directory_uri(); ?>/images/germany.svg" alt="" />
-              <p class="address-content">
-                Christine-Teusch-Anlage 19 <br />67067 Ludwigshafen am Rhein
-                <br />Germany
-              </p>
-            </div>
-          </div>
-
-          <div class="enquiry">
-            <h1 class="footer-heading enquiry-heading-sm d-n">
-              Send your enquiry at
-            </h1>
-
-            <div class="email-input p-rel mb-12">
-              <input placeholder="Enter your email" class="input-wds" type="text" />
-              <img src="<?php echo get_template_directory_uri(); ?>/images/Profile.svg" alt="" />
-              <button class="primary-button">Subscribe</button>
-            </div>
-
-            <p class="mail-id mb-8">Info@arcsinfotech.com</p>
-            <p class="phone-no">+91(172)315-7744</p>
-          </div>
-        </div>
-
-        <div class="second-footer wds">
-          <p class="rights-reserved">
-            ©2024 ARCS Infotech. all rights reserved.
-          </p>
-          <div class="footer-buttons">
-            <button class="firm-logos facebook o-h p-rel cp">
-              <div class="outer-border bx-border-box t-3"></div>
-              <img
-                class="f-with-hover d-n"
-                src="<?php echo get_template_directory_uri(); ?>/images/ic_baseline-facebook.svg"
-                alt=""
-              />
-              <img
-                class="f-no-hover d-b"
-                src="<?php echo get_template_directory_uri(); ?>/images/facebook.svg"
-                alt=""
-              />Facebook
-            </button>
-            <button class="firm-logos instagram o-h p-rel cp">
-              <div class="outer-border bx-border-box t-3"></div>
-              <img
-                class="f-with-hover d-n"
-                src="<?php echo get_template_directory_uri(); ?>/images/ri_instagram-fill.svg"
-                alt=""
-              />
-              <img
-                class="f-no-hover d-b"
-                src="<?php echo get_template_directory_uri(); ?>/images/instagram.svg"
-                alt=""
-              />Instagram
-            </button>
-            <button class="firm-logos linkdin o-h p-rel cp">
-              <div class="outer-border bx-border-box t-3"></div>
-              <img
-                class="f-with-hover d-n"
-                src="<?php echo get_template_directory_uri(); ?>/images/linkedin-svgrepo-com 2.svg"
-                alt=""
-              />
-              <img
-                class="f-no-hover d-b"
-                src="<?php echo get_template_directory_uri(); ?>/images/linkdin.svg"
-                alt=""
-              />linkdin
-            </button>
-          </div>
-        </div>
+      <div class="info">
+        <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M11.7047 0.25C12.7105 0.25 13.678 0.6475 14.3897 1.36075C15.1022 2.0725 15.5005 3.0325 15.5005 4.0375V9.9625C15.5005 12.055 13.798 13.75 11.7047 13.75H4.29549C2.20224 13.75 0.500488 12.055 0.500488 9.9625V4.0375C0.500488 1.945 2.19474 0.25 4.29549 0.25H11.7047ZM12.898 5.155L12.958 5.095C13.1372 4.8775 13.1372 4.5625 12.9497 4.345C12.8455 4.23325 12.7022 4.165 12.553 4.15C12.3955 4.14175 12.2455 4.195 12.1322 4.3L8.75049 7C8.31549 7.36075 7.69224 7.36075 7.25049 7L3.87549 4.3C3.64224 4.1275 3.31974 4.15 3.12549 4.3525C2.92299 4.555 2.90049 4.8775 3.07224 5.1025L3.17049 5.2L6.58299 7.8625C7.00299 8.1925 7.51224 8.3725 8.04549 8.3725C8.57724 8.3725 9.09549 8.1925 9.51474 7.8625L12.898 5.155Z" fill="#202323" />
+        </svg>
+        <p class="inner-content">Info@arcsinfotech.com</p>
       </div>
     </div>
+
+    <div class="service-info">
+      <h2 class="footer-heading-text mb-16">Our Services</h2>
+      <ul>
+        <li class="footer-para-text mb-16">Custom software Development</li>
+        <li class="footer-para-text mb-16">Mobile App consulting</li>
+        <li class="footer-para-text mb-16">Experience Design</li>
+        <li class="footer-para-text mb-16">MVP development</li>
+        <li class="footer-para-text mb-16">Digital Transformation</li>
+        <li class="footer-para-text mb-16">Product Development</li>
+        <li class="footer-para-text mb-16">Dedicated Development team</li>
+        <li class="footer-para-text mb-16">MVP development</li>
+        <li class="footer-para-text mb-16">AI/ML Consulting and Development</li>
+        <li class="footer-para-text mb-16">Application Modernization</li>
+      </ul>
+    </div>
+
+    <div class="service-info">
+      <h2 class="footer-heading-text mb-16">Our technology</h2>
+      <ul>
+        <li class="footer-para-text mb-16">Web app Development</li>
+        <li class="footer-para-text mb-16">Database Development</li>
+        <li class="footer-para-text mb-16">DevOps & cloud</li>
+        <li class="footer-para-text mb-16">Software qA & Testing</li>
+        <li class="footer-para-text mb-16">UI/UX Design </li>
+        <li class="footer-para-text mb-16">mobile app Development</li>
+        <li class="footer-para-text mb-16">ecommerce Development</li>
+      </ul>
+    </div>
+
+    <div class="service-info">
+      <h2 class="footer-heading-text mb-16">
+        Locate Us At
+      </h2>
+      <img src="<?php echo get_template_directory_uri(); ?>/images/india-address.svg" alt="" />
+      <p class="footer-para-text mb-16">
+        Phase 9 Industrial Area <br />Mohali, 160062 <br />India
+      </p>
+
+
+      <div class="address mb-16">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/america.svg" alt="" />
+        <p class="footer-para-text">
+          30 N Gould Street Ste <br />R Sheridan <br />Wyoming 82801
+          <br />US
+        </p>
+      </div>
+
+      <div class="address">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/germany.svg" alt="" />
+        <p class="footer-para-text">
+          Christine-Teusch-Anlage 19 <br />67067 Ludwigshafen am Rhein
+          <br />Germany
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="second-footer wds">
+    <p class="inner-content">
+      ©2024 ARCS Infotech. all rights reserved. <a href="" class="ml-12">Privacy policy</a> <a href="" class="ml-12">Terms and conditions</a>
+    </p>
+    <div class="footer-buttons">
+      <button class="firm-logos facebook o-h p-rel cp">
+        <div class="outer-border bx-border-box t-3"></div>
+        <img
+          class="f-with-hover d-n"
+          src="<?php echo get_template_directory_uri(); ?>/images/ic_baseline-facebook.svg"
+          alt="" />
+        <img
+          class="f-no-hover d-b"
+          src="<?php echo get_template_directory_uri(); ?>/images/facebook.svg"
+          alt="" />Facebook
+      </button>
+      <button class="firm-logos instagram o-h p-rel cp">
+        <div class="outer-border bx-border-box t-3"></div>
+        <img
+          class="f-with-hover d-n"
+          src="<?php echo get_template_directory_uri(); ?>/images/ri_instagram-fill.svg"
+          alt="" />
+        <img
+          class="f-no-hover d-b"
+          src="<?php echo get_template_directory_uri(); ?>/images/instagram.svg"
+          alt="" />Instagram
+      </button>
+      <button class="firm-logos linkdin o-h p-rel cp">
+        <div class="outer-border bx-border-box t-3"></div>
+        <img
+          class="f-with-hover d-n"
+          src="<?php echo get_template_directory_uri(); ?>/images/linkedin-svgrepo-com 2.svg"
+          alt="" />
+        <img
+          class="f-no-hover d-b"
+          src="<?php echo get_template_directory_uri(); ?>/images/linkdin.svg"
+          alt="" />linkdin
+      </button>
+    </div>
+  </div>
+</div>
+</div>
